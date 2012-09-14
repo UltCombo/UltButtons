66ac32bad53019a06a4ad5448a08cdf1c53cd024
UltCombo <ult_combo@hotmail.com>
2012-09-14 09:27:48 -0300

    UltButtons page v0.1

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,61 @@
-console.log('This would be the main JS file.');
+(function($) {
+	$.UltC || ($.UltC = {});
+
+	$.UltC.unselectableSupport = document.createElement('div').hasOwnProperty('unselectable');
+
+	$.fn.disableSelection = function() {
+		if ($.UltC.unselectableSupport) this.contents().andSelf().prop('unselectable', 'on'); //Opera
+		return this
+			.css('user-select', 'none') //FF, Chrome
+			.on('selectstart', false); //IE
+    };
+
+	$.fn.ultButtonset = function() {
+		this.buttonset.apply(this, arguments);
+		this.find('label').disableSelection();
+		return this;
+	}
+	$.fn.ultButton = function() {
+		this.button.apply(this, arguments);
+		return this.each(function() {
+			$('label[for="' + this.id + '"]').disableSelection();
+		});
+	}
+
+	$(document).mousedown(function(e) {
+		if (e.which !== 1) return false;
+		$.UltC.mdtarg = e.target;
+	}).on('mousedown', 'label.ui-button', function(e) {
+		if (e.which !== 1) return false;
+		var $this = $(this);
+		$this.data('checked', $this.prev()[0].checked);
+	}).on('mouseup', 'label.ui-button', function(e) {
+		if (e.which !== 1) return false;
+		var $this = $(this),
+			$ch = $this.prev(),
+			ch = $ch[0];
+		setTimeout(function() {
+			if (ch.disabled) return false;
+			var targ = $.UltC.mdtarg;
+			if ($this.data('checked') === ch.checked && ($this.has(targ).length || targ === $this[0])) {
+				if ($ch.is('[type="radio"]')) {
+					if (!ch.checked) {
+						ch.checked = true;
+						$('input[type="radio"][name="' + ch.name + '"]').not(ch).each(function() {
+							$('label[for="' + this.id + '"]').removeClass('ui-state-active').attr('aria-pressed', false);
+						});
+						$this.addClass('ui-state-active').attr('aria-pressed', true);
+						$ch.trigger('change');
+					}
+				} else {
+					var inversechecked = !ch.checked;
+					ch.checked = inversechecked;
+					if (inversechecked) $this.addClass('ui-state-active');
+					else $this.removeClass('ui-state-active');
+					$this.attr('aria-pressed', inversechecked);
+					$ch.trigger('change');
+				}
+			}
+		}, 0);
+	});
+})(jQuery);
