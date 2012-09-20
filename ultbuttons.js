fedef5f682592a9dbb60b876ed538b7620433a0e
UltCombo <ult_combo@hotmail.com>
2012-09-20 12:46:35 -0300

    setTimeout only when necessary

--- ultbuttons.js
+++ ultbuttons.js
@@ -36,33 +36,37 @@
 		if (e.which !== 1) return;
 		var $this = $(this),
 			$ch = $('input[id="'+ $this.attr('for') + '"]'),
-			ch = $ch[0];
-		setTimeout(function() {
-			if (ch.disabled) return;
-			var targ = $.UltC.mdtarg;
-			//usually, the click target is the span inside the label.ui-button hence the `$this.has(targ).length`. It also takes care of user-created elements inside the label e.g. images
-			//but if the user manages to click in the default 1px border of the label, the target will be the label element itself: targ === $this[0]
-			//if the ui button firing the mouseup handler is the same that triggered the last mousedown handler and the checked state didn't change after
-			//the jQuery UI handled the events (hence the setTimeout), we do the magic
-			if ($this.data('checked') === ch.checked && ($this.has(targ).length || targ === $this[0])) {
-				if (ch.type === 'radio') {
-					if (!ch.checked) {
-						ch.checked = true;
-						$('input[type="radio"][name="' + ch.name + '"]').not(ch).each(function() {
-							$('label[for="' + this.id + '"]').removeClass('ui-state-active').attr('aria-pressed', false);
-						});
-						$this.addClass('ui-state-active').attr('aria-pressed', true);
+			ch = $ch[0],
+			targ = $.UltC.mdtarg;
+
+		if (ch.disabled) return;
+
+		//usually, the click target is the span inside the label.ui-button: `$this.has(targ).length`. It also takes care of user-created elements inside the label e.g. images
+		//but if the user manages to click in the default 1px border of the label, the target will be the label element itself: `targ === $this[0]`
+		if ($this.has(targ).length || targ === $this[0]) {
+			setTimeout(function() {
+				//if the ui button firing the mouseup handler is the same that triggered the last mousedown handler and the checked state didn't change after
+				//the jQuery UI handled the events (hence the setTimeout), we do the magic
+				if ($this.data('checked') === ch.checked) {
+					if (ch.type === 'radio') {
+						if (!ch.checked) {
+							ch.checked = true;
+							$('input[type="radio"][name="' + ch.name + '"]').not(ch).each(function() {
+								$('label[for="' + this.id + '"]').removeClass('ui-state-active').attr('aria-pressed', false);
+							});
+							$this.addClass('ui-state-active').attr('aria-pressed', true);
+							$ch.trigger('change');
+						}
+					} else {
+						var inversechecked = !ch.checked;
+						ch.checked = inversechecked;
+						if (inversechecked) $this.addClass('ui-state-active');
+						else $this.removeClass('ui-state-active');
+						$this.attr('aria-pressed', inversechecked);
 						$ch.trigger('change');
 					}
-				} else {
-					var inversechecked = !ch.checked;
-					ch.checked = inversechecked;
-					if (inversechecked) $this.addClass('ui-state-active');
-					else $this.removeClass('ui-state-active');
-					$this.attr('aria-pressed', inversechecked);
-					$ch.trigger('change');
 				}
-			}
-		}, 0);
+			}, 0);
+		}
 	});
 })(jQuery);
