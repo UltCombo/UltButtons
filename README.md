ab936f1ff6656c484693f23e0139c82188422629
UltCombo <ult_combo@hotmail.com>
2012-12-26 23:32:19 -0200

    v1.1pre

--- README.md
+++ README.md
@@ -20,17 +20,31 @@ Check out the [Project Page](http://ultcombo.github.com/UltButtons/)!
 ##How to use
 Include jQuery, jQuery UI and then ultbuttons.js as follows:
 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
-    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.js"></script>
-    <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/themes/trontastic/jquery-ui.css" />
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.js"></script>
+    <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/trontastic/jquery-ui.css" />
     <script src="ultbuttons1.01.min.js"></script>
 
 Then just call the improved `.ultButton()`/`.ultButtonset()` instead of the native jQuery UI's `.button()`/`.buttonset()`.
 
 ##Notes
 
-- Calling `.ultButton()`/`.ultButtonset()` on actual buttons won't have any improvement effect, as `.button()`/`.buttonset()` works as it should on actual buttons. This plugin is intended for Checkboxes/Radio UI buttons, but calling it on actual buttons won't break anything either. `=]`
+- Calling `.ultButton()`/`.ultButtonset()` on actual buttons won't have any improvement effect, as `.button()`/`.buttonset()` works as it should on actual buttons. This plugin is intended for checkbox/radio UI buttons, but calling it on actual buttons won't break anything either. `=]`
 - **UltButtons** does not create any extra markup, instead, it works on top of the original jQuery UI `button`/`buttonset` methods.
-- The plugin also does not use any globals, it creates a namespace inside the jQuery object to store the necessary data.
-- It adds 3 listeners to the `document`, so if you ever need to call `$(document).off('mousedown')` or such, specify a named function handler to remove in the second parameter, to warrant that it won't break the plugin functionality.
-- The plugin requires jQuery 1.7 at least, recommended is 1.8+
+- The plugin also does not use any globals.
+- It adds 3 listeners to the `document`, so if you ever need to call `$(document).off('event')`, make sure to specify a named function handler to remove in the second parameter or use namespaced handlers, to avoid breaking the plugin functionality.
+- The plugin requires jQuery 1.7 at least, recommended is 1.8+.
+
+## Changelog
+
+### 1.1
+- disableSelection: Fixed unselectable property for nested elements inside buttons in Opera;
+- disableSelection: Fixed userSelect for Firefox and Chrome when using jQuery 1.7;
+- buttonset/disableSelection: Fixed the buttonset method to don't assume that all labels inside a buttonset container are checkbox/radio button widgets;
+- Unobstrusiveness: No longer utilizes `.data()` to store checked state; removed the `$.UltC` namespace. Still adds the `disableSelection` method to the jQuery object prototype for backwards compatibility with older versions which also exposed it.
+
+### 1.01
+- Micro-optimizations.
+
+### 1.0
+- Initial release.
--- ultbuttons.js
+++ ultbuttons.js
@@ -1,18 +1,45 @@
-/*jshint curly:false, strict:false, browser:true, jquery:true */
+/*jshint curly:false, browser:true, jquery:true */
 (function($) {
-	$.UltC = $.UltC || {};
+	'use strict';
+	
+	if (!$.fn.on) throw 'UltButtons requires jQuery 1.7 at least.';
 
-	var helperDiv = document.createElement('div');
-	$.UltC.unselectableSupport = helperDiv.hasOwnProperty && helperDiv.hasOwnProperty('unselectable');
+	var helperDiv = document.createElement('div'),
+		//setting the unselectable property on all descendants is rather costly and supported only in Opera and IE,
+		//hence we do feature detection to don't apply it if it is not supported at all
+		unselectableSupport = helperDiv.hasOwnProperty && helperDiv.hasOwnProperty('unselectable'),
+		//jQuery 1.8+ does the prefix normalization for userSelect automatically, but as we support jQuery 1.7 which
+		//doesn't do the prefixing, we use feature detection.
+		//Unlike jQuery 1.8's internal vendorPropName function, our function returns false instead of the original name,
+		//this way we don't apply the .css() method in case userSelect isn't supported
+		userSelectSupport = (function() {
+			var prop = 'userSelect',
+				st = helperDiv.style;
+			if (prop in st) return prop;
+
+			var capProp = 'UserSelect',
+				prefixes = ['Moz', 'Webkit', 'O', 'ms'];
+			
+			for (var i = 0; i < prefixes.length; i++) {
+				prop = prefixes[i] + capProp;
+				if (prop in st) {
+					return prop;
+				}
+			}
+			return false;
+		}()),
+		mdtarg, mdchecked;
+	helperDiv = null;
 
 	$.fn.disableSelection = function() {
-		if ($.UltC.unselectableSupport) this.contents().andSelf().prop('unselectable', 'on'); //unselectable for Opera
-		return this.css('user-select', 'none').on('selectstart', false); //user-select for FF, Chrome; selectstart for IE
+		if (unselectableSupport) this.find('*').andSelf().prop('unselectable', 'on'); //unselectable for Opera
+		if (userSelectSupport) this.css(userSelectSupport, 'none'); //user-select for FF, Chrome
+		return this.on('selectstart', false); //selectstart for IE
 	};
 
 	$.fn.ultButtonset = function() {
 		this.buttonset.apply(this, arguments);
-		this.find('label').disableSelection();
+		this.find('label.ui-button').disableSelection();
 		return this;
 	};
 	$.fn.ultButton = function() {
@@ -24,43 +51,37 @@
 
 	$(document).mousedown(function(e) {
 		if (e.which !== 1) return;
-		$.UltC.mdtarg = e.target;
+		mdtarg = e.target;
 	}).on('mousedown', 'label.ui-button', function(e) {
 		if (e.which !== 1) return;
-		var $this = $(this);
 		//the following selector is used to support eccentric ids, e.g. `input[id="foo.bar"]`.
 		//we store the checked state of the input in the label during the mousedown event to check agaisnt on mouseup
-		$this.data('checked', $('input[id="'+ $this.attr('for') + '"]').prop('checked'));
+		mdchecked = $('input[id="'+ $(this).attr('for') + '"]')[0].checked;
 	}).on('mouseup', 'label.ui-button', function(e) {
 		if (e.which !== 1) return;
 		var $this = $(this),
 			$ch = $('input[id="' + $this.attr('for') + '"]'),
-			ch = $ch[0],
-			targ = $.UltC.mdtarg;
+			ch = $ch[0];
 
 		if (ch.disabled) return;
 
 		//usually, the click target is the span inside the label.ui-button: `$this.has(targ).length`. It also takes care of user-created elements inside the label e.g. images
 		//but if the user manages to click in the default 1px border of the label, the target will be the label element itself: `targ === $this[0]`
-		if ($this.has(targ).length || targ === $this[0]) {
+		if ($this.has(mdtarg).length || mdtarg === $this[0]) {
 			setTimeout(function() {
 				//if the ui button firing the mouseup handler is the same that triggered the last mousedown handler and the checked state didn't change after
 				//the jQuery UI handled the events (hence the setTimeout), we do the magic
-				if ($this.data('checked') === ch.checked) {
+				if (mdchecked === ch.checked) {
 					if (ch.type === 'checkbox') {
 						var inversechecked = !ch.checked;
 						ch.checked = inversechecked;
-						if (inversechecked) $this.addClass('ui-state-active');
-						else $this.removeClass('ui-state-active');
-						$this.attr('aria-pressed', inversechecked);
-						$ch.trigger('change');
+						$this.toggleClass('ui-state-active', inversechecked).attr('aria-pressed', inversechecked);
+						$ch.change();
 					} else if (ch.type === 'radio' && !ch.checked) {
+						$('input[type="radio"][name="' + ch.name + '"]:checked').button('widget').removeClass('ui-state-active').attr('aria-pressed', false);
 						ch.checked = true;
-						$('input[type="radio"][name="' + ch.name + '"]').not(ch).each(function() {
-							$('label[for="' + this.id + '"]').removeClass('ui-state-active').attr('aria-pressed', false);
-						});
 						$this.addClass('ui-state-active').attr('aria-pressed', true);
-						$ch.trigger('change');
+						$ch.change();
 					}
 				}
 			}, 0);
